983b0d434df8f00c6edafe8c96dbd35db43a4e1f
Anton Klock <anton@klockworks.se>
2024-11-26 11:36:04 +0100

    Changing to fall back to port 8080

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 8080;
 app.use(express.static('public'));
 app.use(express.json());
 
